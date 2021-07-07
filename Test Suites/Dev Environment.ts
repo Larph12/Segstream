71727c4aa8797a9df88ba4bf5249c57da2c5569c
ralphjasmin <66618746+ralphjasmin@users.noreply.github.com>
2021-07-08 03:23:39 +0800

    New Modifications Change-Id: I73aca27e55d140b94eaaca11b5655eb8fed664ba Signed-off-by: ralphninojasmin@gmail.com

---
+++ Test Suites/Dev Environment.ts
@@ -0,0 +1,128 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Dev Environment</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>768c9ca3-d5a5-4910-9578-ae917b6735a1</testSuiteGuid>
+   <testCaseLink>
+      <guid>93e1c481-3978-4720-8428-20a74e7c60e9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dev Test Cases/Add New Project - NonEPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dc7da954-7f3f-4d4d-a1e8-ef89ea98ffcb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dev Test Cases/Add New Project</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9e05eb61-5bbd-43cd-8153-824eeabf07f1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dev Test Cases/Add New Property - NonEPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6bd51993-7154-4d4c-9dfd-94ca8ed5f551</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dev Test Cases/Bulk Select -Inverse EPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4412b589-dbaa-4734-9fd1-61ad2c665a7e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dev Test Cases/Add Property-EPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>29f60ec5-424f-4389-a574-7acf08be36f1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dev Test Cases/Bulk Select By Asset Class -EPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>341781ba-188b-4027-ba9d-976c5c3de9c7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dev Test Cases/Bulk Select By Location -EPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>45635c9c-03d2-4172-a34f-420d165b17a9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dev Test Cases/Bulk Select By Recovery Period -EPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8caf6dcd-2ceb-47be-bb9d-55464646c494</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dev Test Cases/Bulk Select EPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a3be011a-83eb-4102-b1f3-2fbac0417c5b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dev Test Cases/Bulk Select NonEPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>21f82e71-20db-4b7d-b969-cc87fdb429ad</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dev Test Cases/Direct Cost Test case-EPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d6fe2ecf-984d-4077-bdda-2ac1226df794</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dev Test Cases/Updating Worksheet data-EPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>04c7d40d-5296-4f7a-a442-8c1e49f85872</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dev Test Cases/User Sigin - EPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d1e0375c-a2d1-41b3-9188-073ca6d04320</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dev Test Cases/User Sigin-NonEPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b5cccc68-9d77-4f0c-864c-8e6c5b0d7824</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dev Test Cases/Verify Left Panel Buttons</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7f7ba678-4ad7-4c22-80fd-913a3584b3bf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dev Test Cases/Verify Top Panel Buttons</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7fae1ddf-d54d-4725-8e54-e2ce1b7a33bb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dev Test Cases/Viewing a worksheet-EPS - Copy</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>102ce2c1-88e7-4864-b121-239dbdb27cc7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dev Test Cases/Viewing a worksheet-EPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ef37a8c9-4582-4fc1-9640-627fabd55bea</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dev Test Cases/Viewing a worksheet-NonEPS</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Staging Environment.ts
@@ -0,0 +1,128 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Staging Environment</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ae669468-9000-4e19-a598-b6f8a3e946ca</testSuiteGuid>
+   <testCaseLink>
+      <guid>b9546605-4b4d-4a85-8d33-39ea14280fa7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Staging Test Cases/Add New Project - NonEPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d1d061e1-0d27-4860-a628-e78ff3c2fea4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Staging Test Cases/Add New Project</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4e018e2b-74d8-4185-9060-a1827d5b6307</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Staging Test Cases/Add New Property - NonEPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c79001af-a515-4626-98c5-831873dd7c40</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Staging Test Cases/Add Property-EPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5ae26d6c-347b-4578-8074-1d99cc88cf28</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Staging Test Cases/Bulk Select -Inverse EPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f6aff211-5d72-4f41-a8bf-e616e4a66b90</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Staging Test Cases/Bulk Select By Asset Class -EPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>98eb3920-aeff-4bbc-b799-506f03e78b95</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Staging Test Cases/Bulk Select By Location -EPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>777be2c1-43f7-47a0-9746-d963acd6ce7d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Staging Test Cases/Bulk Select By Recovery Period -EPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d00a224e-3a97-48e7-a897-064f6782a781</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Staging Test Cases/Bulk Select EPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0a8cd096-ab7e-47f2-a2f5-1a69005b13d6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Staging Test Cases/Bulk Select NonEPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4e37247e-2153-4a18-a12e-88a1540a6107</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Staging Test Cases/Direct Cost Test case-EPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b309df5a-4933-43a6-9478-5c151bd9e97c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Staging Test Cases/Updating Worksheet data-EPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a6d6ff37-c1fc-4be9-8d0b-aca115764eff</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Staging Test Cases/User Sigin - EPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>02bc651a-c308-49ad-95d1-6ba64c41b377</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Staging Test Cases/User Sigin-NonEPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>aa3c41e6-3cbb-4829-9209-43fdce7ab1c2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Staging Test Cases/Verify Left Panel Buttons</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a85f5bf3-ec72-4b07-973c-68a74abbe2a6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Staging Test Cases/Verify Top Panel Buttons</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>692bde8e-5f64-418c-a755-0da265c755af</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Staging Test Cases/Viewing a worksheet-EPS - Copy</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>947629d3-dd9a-423e-ba60-ac377dc85023</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Staging Test Cases/Viewing a worksheet-EPS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7dc82f8e-8761-4479-853c-f97c88aa9b08</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Staging Test Cases/Viewing a worksheet-NonEPS</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
